53163182b0f96d7d5050981c58aa6eddf3460b90
nevaao <164090419+nevaao@users.noreply.github.com>
2024-03-19 20:35:24 -0300

    Update HomePageObject.js

--- cypress/e2e/pageobjects/HomePageObject.js
+++ cypress/e2e/pageobjects/HomePageObject.js
@@ -10,21 +10,34 @@ class HomePageObject {
 
     // Clica no botão para criar uma nova conta
     createAccount() {
+        cy.wait(1000);
         ButtonCreateAccount().click();
     }
     
     // Clica no botão para realizar o login
     signIn() {
+        cy.wait(1000);
         ButtonSignIn().click();
     }
 
     // Acessa o menu Men
     accessMenu(menu) {
+        cy.wait(1000);
         switch (menu) {
             case "Men": {
                 MenuMen().click();
             }
         }
+    }
+
+    // Valida se o usuário está na Home Page
+    validateHomePage() {
+        cy.get("main.page-main", { timeout: 30000 }).should("exist");
+    }
+
+    // Valida a mensagem de boas-vindas
+    validateWelcome(mensagemBoasVindas) {
+        cy.get(".panel span[class = 'logged-in']", { timeout: 30000 }).should("contain.text", mensagemBoasVindas);
     }
 }
 
