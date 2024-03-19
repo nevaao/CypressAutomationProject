ad3c42fa982c6e4a62738ed40b3b8ac4a4945f26
nevaao <164090419+nevaao@users.noreply.github.com>
2024-03-19 20:36:07 -0300

    Update MyAccountPageObject.js

--- cypress/e2e/pageobjects/MyAccountPageObject.js
+++ cypress/e2e/pageobjects/MyAccountPageObject.js
@@ -1,7 +1,7 @@
 class MyAccountPageObject {
     // Valida se a conta foi criada com sucesso
     validateSuccessMessage(mensagem) {
-        cy.get(".message-success > div").should("contain.text", mensagem);
+        cy.get(".message-success > div", { timeout: 3000 }).should("contain.text", mensagem);
     }
 }
 
