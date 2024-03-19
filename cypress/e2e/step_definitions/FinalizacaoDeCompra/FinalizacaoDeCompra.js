11bd0064074f11c97bec2c98f577b19e0acb6d7b
nevaao <164090419+nevaao@users.noreply.github.com>
2024-03-19 20:40:55 -0300

    Update FinalizacaoDeCompra.js

--- cypress/e2e/step_definitions/FinalizacaoDeCompra/FinalizacaoDeCompra.js
+++ cypress/e2e/step_definitions/FinalizacaoDeCompra/FinalizacaoDeCompra.js
@@ -11,61 +11,49 @@ Given('que o usuário acessa o sistema', () => {
 })
 
 Given('que o usuário acessa a menu {string}', menu => {
-    cy.wait(1000);
     HomePageObject.accessMenu(menu);
 })
 
 Given('que o usuário acessa a categoria {string}', categoria => {
-    cy.wait(1000);
     MenPageObject.accessCategory(categoria);
 })
 
 When('o usuário seleciona o produto {string}', produto => {
-    cy.wait(1000);
     MenPageObject.selectProduct(produto);
 })
 
 When('o usuário seleciona as opções do produto', opcoesProduto => {
-    cy.wait(1000);
     ProductPageObject.selectProductOptions(opcoesProduto);
 })
 
 When('o usuário adiciona o produto ao carrinho', () => {
-    cy.wait(1000);
     ProductPageObject.addToCart();
 })
 
 When('o usuário visualiza a mensagem de produto adicionado com sucesso {string}', (mensagem) => {
-    cy.wait(1000);
-    cy.get("div[data-ui-id='message-success'] div").should("contain.text", mensagem);
+    ProductPageObject.validateSuccessMessage(mensagem);
 })
 
 When('o usuário solicita para fazer o checkout do pedido', () => {
-    cy.wait(1000);
     ProductPageObject.checkoutOrder();
 })
 
 When('o usuário preenche nos campos do endereço de entrega', (camposEnderecoEntrega) => {
-    cy.get('#shipping > .step-title', { timeout: 60000 }).should("exist");
     ShippingInfosPageObject.fillInFields(camposEnderecoEntrega);
 })
 
 When('o usuário solicita para avançar para a próxima etapa', () => {
-    cy.wait(1000);
     ShippingInfosPageObject.next();
 })
 
 When('o usuário solicita para finalizar a compra', () => {
-    cy.get('.payment-group > .step-title', { timeout: 60000 }).should("exist");
     PaymentInfosPageObject.placeOrder();
 })
 
 Then('o usuário visualiza a mensagem de compra com sucesso {string}', mensagem => {
-    cy.wait(1000);
-    cy.get("span[data-ui-id='page-title-wrapper']").should("contain.text", mensagem)
+    PurchaseConfirmationPageObject.validateSuccessMessage(mensagem);
 })
 
 Then('o usuário visualiza as opções', opcoes => {
-    cy.wait(1000);
     PurchaseConfirmationPageObject.validateOptions(opcoes);
 })
