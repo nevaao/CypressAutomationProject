689a8f01c5bb3956bdf9202c7fa8727c3ecf09ba
nevaao <164090419+nevaao@users.noreply.github.com>
2024-03-19 20:35:45 -0300

    Update MenPageObject.js

--- cypress/e2e/pageobjects/MenPageObject.js
+++ cypress/e2e/pageobjects/MenPageObject.js
@@ -1,11 +1,13 @@
 class MenPageObject {
     // Acessa a categoria de produto desejada
     accessCategory(categoria) {
+        cy.wait(1000);
         cy.get(`div.categories-menu a[href*='tops-men/${categoria.toLowerCase()}']`).click();
     }
 
     // Seleciona o produto
     selectProduct(produto) {
+        cy.wait(1000);
         cy.get(`a.product-item-link[href*='${produto.replaceAll(' ', '-').toLowerCase()}']`).click();
     }
 }
