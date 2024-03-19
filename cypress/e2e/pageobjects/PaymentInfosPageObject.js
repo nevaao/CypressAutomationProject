c9c70a54aa4cf51568394b2538efc6038ae34de0
nevaao <164090419+nevaao@users.noreply.github.com>
2024-03-19 20:36:24 -0300

    Update PaymentInfosPageObject.js

--- cypress/e2e/pageobjects/PaymentInfosPageObject.js
+++ cypress/e2e/pageobjects/PaymentInfosPageObject.js
@@ -3,6 +3,7 @@ const PlaceOrder = () => cy.get("button[title='Place Order']");
 class PaymentInfosPageObject {
     // Finaliza a compra
     placeOrder() {
+        cy.get('.payment-group > .step-title', { timeout: 30000 }).should("exist");
         PlaceOrder().click();
     }
 }
