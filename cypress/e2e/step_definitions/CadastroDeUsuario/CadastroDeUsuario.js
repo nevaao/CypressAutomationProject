0b934ccc5f294bb0d59f32cae01e2696a9bd8d64
nevaao <164090419+nevaao@users.noreply.github.com>
2024-03-19 20:39:35 -0300

    Update CadastroDeUsuario.js

--- cypress/e2e/step_definitions/CadastroDeUsuario/CadastroDeUsuario.js
+++ cypress/e2e/step_definitions/CadastroDeUsuario/CadastroDeUsuario.js
@@ -8,21 +8,17 @@ Given('que o usuário acessa o sistema', () => {
 })
 
 Given('que o usuário solicita para cadastrar uma nova conta', () => {
-    cy.wait(1000);
     HomePageObject.createAccount();
 })
 
 When('o usuário preenche nos campos da tela de cadastro', (camposCadastro) => {
-    cy.wait(1000);
     CadastroPageObject.fillInFields(camposCadastro);
 })
 
 When('o usuário confirma o cadastro da conta', () => {
-    cy.wait(1000);
     CadastroPageObject.confirmCreateAccount();
 })
 
 Then('o usuário visualiza a mensagem de conta cadastrada com sucesso {string}', mensagem => {
-    cy.wait(1000);
     MyAccountPageObject.validateSuccessMessage(mensagem);
 })
