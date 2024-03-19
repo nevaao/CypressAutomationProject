3a5558b5dbc80ec3d11de2dc6f8ea1c7a516ed3b
nevaao <164090419+nevaao@users.noreply.github.com>
2024-03-19 20:41:13 -0300

    Update Login.js

--- cypress/e2e/step_definitions/Login/Login.js
+++ cypress/e2e/step_definitions/Login/Login.js
@@ -7,26 +7,21 @@ Given('que o usuário acessa o sistema', () => {
 })
 
 Given('que o usuário solicita para fazer o login', () => {
-    cy.wait(1000);
     HomePageObject.signIn();
 })
 
 When('o usuário preenche nos campos da tela de login', (camposLogin) => {
-    cy.wait(1000);
     CustomerLoginPageObject.fillInFields(camposLogin);
 })
 
 When('o usuário confirma o login', () => {
-    cy.wait(1000);
     CustomerLoginPageObject.signIn();
 })
 
 Then('o usuário é redirecionado para a tela inicial', () => {
-    cy.wait(1000);
-    cy.get('#ui-id-3 > span').should("exist");
+    HomePageObject.validateHomePage();
 })
 
 Then('o usuário visualiza no cabeçalho da página {string}', mensagemBoasVindas => {
-    cy.wait(1000);
-    cy.get(".panel span[class = 'logged-in']").should("contain.text", mensagemBoasVindas);
+    HomePageObject.validateWelcome(mensagemBoasVindas);
 })
