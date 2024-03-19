e8995ad712ac64c3e3784bee55ad6482e37c119b
nevaao <164090419+nevaao@users.noreply.github.com>
2024-03-19 20:39:06 -0300

    Update AdicionarProdutoAoCarrinho.js

--- cypress/e2e/step_definitions/AdicionarProdutoAoCarrinho/AdicionarProdutoAoCarrinho.js
+++ cypress/e2e/step_definitions/AdicionarProdutoAoCarrinho/AdicionarProdutoAoCarrinho.js
@@ -8,36 +8,29 @@ Given('que o usuário acessa o sistema', () => {
 })
 
 Given('que o usuário acessa a menu {string}', menu => {
-    cy.wait(1000);
     HomePageObject.accessMenu(menu);
 })
 
 Given('que o usuário acessa a categoria {string}', categoria => {
-    cy.wait(1000);
     MenPageObject.accessCategory(categoria);
 })
 
 When('o usuário seleciona o produto {string}', produto => {
-    cy.wait(1000);
     MenPageObject.selectProduct(produto);
 })
 
 When('o usuário seleciona as opções do produto', opcoesProduto => {
-    cy.wait(1000);
     ProductPageObject.selectProductOptions(opcoesProduto);
 })
 
 When('o usuário adiciona o produto ao carrinho', () => {
-    cy.wait(1000);
     ProductPageObject.addToCart();
 })
 
 Then('o usuário visualiza a mensagem de produto adicionado com sucesso {string}', (mensagem) => {
-    cy.wait(1000);
-    cy.get("div[data-ui-id='message-success'] div").should("contain.text", mensagem);
+    ProductPageObject.validateSuccessMessage(mensagem);
 })
 
 Then('o usuário visualiza o carrinho com {string} produto', (quantidadeProdutos) => {
-    cy.wait(1000);
     ProductPageObject.validateCart(quantidadeProdutos);
 })
